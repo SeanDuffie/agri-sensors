819cf6d55055fd8b924f56188b1934ce98686650
SeanDuffie <sean.duffie@tamu.edu>
2022-09-13 09:39:36 -0500

    Adding Water Pump Functionality with different serial commands

--- agri-sensors.ino
+++ agri-sensors.ino
@@ -2,10 +2,12 @@
 
 #define DHT11_PIN 7
 dht DHT;
-int LIGHT_PIN = A0; // LIGHT SENSOR
+#define LIGHT_PIN A0 // LIGHT SENSOR
 float LIGHT_VAL = 0;
-int SOIL_PIN = A1; // SOIL SENSOR
+#define SOIL_PIN A1 // SOIL SENSOR
 float SOIL_VAL = 0;
+#define SPEAKER 2     // SPEAKER OUTPUT
+#define WATER_PUMP 4  // WATER PUMP OUTPUT
 
 #define DEBUG 1
 #if DEBUG == 1
@@ -16,7 +18,11 @@ float SOIL_VAL = 0;
 #define debugln(x)
 #endif
 
-void setup() { Serial.begin(115200); }
+void setup() {
+  Serial.begin(115200);
+  pinMode(SPEAKER, OUTPUT);
+  pinMode(WATER_PUMP, OUTPUT);
+}
 
 void loop() {
   if (Serial.available()) {
@@ -34,10 +40,35 @@ void loop() {
       debug(DHT.temperature);
       debug(",");
       debug(DHT.humidity); // debugln??
-    } else if (measureReq.equals("10")) { // Do nothing, this is a return character
+    } else if (measureReq.equals("50")) {
+      digitalWrite(SPEAKER, HIGH);
+      delay(500);
+      digitalWrite(SPEAKER, LOW);
+      debug(measureReq);
+    } else if (measureReq.equals("51")) {
+      digitalWrite(SPEAKER, HIGH);
+      delay(100);
+      digitalWrite(SPEAKER, LOW);
+      delay(100);
+      digitalWrite(SPEAKER, HIGH);
+      delay(100);
+      digitalWrite(SPEAKER, LOW);
+      digitalWrite(WATER_PUMP, HIGH);
+      debug(measureReq);
+    } else if (measureReq.equals("52")) {
+      digitalWrite(WATER_PUMP, LOW);
+      digitalWrite(SPEAKER, HIGH);
+      delay(100);
+      digitalWrite(SPEAKER, LOW);
+      delay(100);
+      digitalWrite(SPEAKER, HIGH);
+      delay(100);
+      digitalWrite(SPEAKER, LOW);
+      debug(measureReq);
+    } else if (measureReq.equals("10")) { debug(measureReq);  // Do nothing, this is a return character
     } else {
-      debug("Invalid Request: ");
-      debugln(measureReq);
+//      debug("Invalid Request: ");
+      debug(measureReq);
     }
   } else {}
 }
